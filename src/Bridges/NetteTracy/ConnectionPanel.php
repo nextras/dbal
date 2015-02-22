2b88392777f6f62ddde1288d4606cc6414a85244
hrach <hrach.cz@gmail.com>
2015-02-22 14:52:58 +0100

    ConnectionPanel: added sql highlighting

--- src/Bridges/NetteTracy/ConnectionPanel.php
+++ src/Bridges/NetteTracy/ConnectionPanel.php
@@ -66,10 +66,35 @@ class ConnectionPanel implements IBarPanel
 	{
 		$count = $this->count;
 		$queries = $this->queries;
+		$queries = array_map(function($row) {
+			$row[1] = self::highlight($row[1]);
+			return $row;
+		}, $queries);
 
 		ob_start();
 		require __DIR__ . '/ConnectionPanel.panel.phtml';
 		return ob_get_clean();
+	}
+
+
+	public static function highlight($sql)
+	{
+		static $keywords1 = 'SELECT|(?:ON\s+DUPLICATE\s+KEY)?UPDATE|INSERT(?:\s+INTO)?|REPLACE(?:\s+INTO)?|SHOW|DELETE|CALL|UNION|FROM|WHERE|HAVING|GROUP\s+BY|ORDER\s+BY|LIMIT|OFFSET|SET|VALUES|LEFT\s+JOIN|INNER\s+JOIN|TRUNCATE';
+		static $keywords2 = 'ALL|DISTINCT|DISTINCTROW|IGNORE|AS|USING|ON|AND|OR|IN|IS|NOT|NULL|[RI]?LIKE|REGEXP|TRUE|FALSE';
+
+		$sql = " $sql ";
+		$sql = htmlSpecialChars($sql, ENT_IGNORE, 'UTF-8');
+		$sql = preg_replace_callback("#(/\\*.+?\\*/)|(?<=[\\s,(])($keywords1)(?=[\\s,)])|(?<=[\\s,(=])($keywords2)(?=[\\s,)=])#is", function($matches) {
+			if (!empty($matches[1])) { // comment
+				return '<em style="color:gray">' . $matches[1] . '</em>';
+			} elseif (!empty($matches[2])) { // most important keywords
+				return '<strong style="color:#2D44AD">' . $matches[2] . '</strong>';
+			} elseif (!empty($matches[3])) { // other keywords
+				return '<strong>' . $matches[3] . '</strong>';
+			}
+		}, $sql);
+
+		return trim($sql);
 	}
 
 }
