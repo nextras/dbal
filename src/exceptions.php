560d82a701071dccfa6523423eefe951b327606c
hrach <hrach.cz@gmail.com>
2015-04-12 18:01:14 +0200

    exceptions merged into one file

---
+++ src/exceptions.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * This file is part of the Nextras\Dbal library.
+ * @license    MIT
+ * @link       https://github.com/nextras/dbal
+ */
+
+namespace Nextras\Dbal\Exceptions;
+
+use Exception;
+
+
+class InvalidArgumentException extends \InvalidArgumentException
+{
+}
+
+
+class InvalidStateException extends \LogicException
+{
+}
+
+
+class IOException extends \RuntimeException
+{
+}
+
+
+class NotImplementedException extends \LogicException
+{
+}
+
+
+class NotNullConstraintViolationException extends ConstraintViolationException
+{
+}
+
+
+class NotSupportedException extends \LogicException
+{
+}
+
+
+class ConnectionException extends DriverException
+{
+}
+
+
+
+
+abstract class ConstraintViolationException extends QueryException
+{
+}
+
+
+class ForeignKeyConstraintViolationException extends ConstraintViolationException
+{
+}
+
+
+class UniqueConstraintViolationException extends ConstraintViolationException
+{
+}
+
+
+
+
+class DriverException extends Exception
+{
+	/** @var int */
+	private $errorCode;
+
+	/** @var string */
+	private $errorSqlState;
+
+
+	/**
+	 * @param string    $message
+	 * @param int       $errorCode
+	 * @param string    $errorSqlState
+	 * @param Exception $previousException
+	 */
+	public function __construct($message, $errorCode = 0, $errorSqlState = '', Exception $previousException = NULL)
+	{
+		parent::__construct($message, 0, $previousException);
+		$this->errorCode = (int) $errorCode;
+		$this->errorSqlState = $errorSqlState;
+	}
+
+
+	public function getErrorCode()
+	{
+		return $this->errorCode;
+	}
+
+
+	public function getErrorSqlState()
+	{
+		return $this->errorSqlState;
+	}
+
+}
+
+
+class QueryException extends DriverException
+{
+	/** @var string */
+	private $sqlQuery;
+
+
+	public function __construct($message, $errorCode = 0, $errorSqlState = '', $previousException = NULL, $sqlQuery = NULL)
+	{
+		parent::__construct($message, $errorCode, $errorSqlState, $previousException);
+		$this->sqlQuery = (string) $sqlQuery;
+	}
+
+
+	public function getSqlQuery()
+	{
+		return $this->sqlQuery;
+	}
+
+}
