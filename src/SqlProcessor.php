07ad1e2a619514d33ce92ee74b0e0fb469bdcff2
Vít Kutný <vitkutny@users.noreply.github.com>
2015-09-18 19:42:14 +0200

    SqlProcessor: insert default values

--- src/SqlProcessor.php
+++ src/SqlProcessor.php
@@ -370,10 +370,10 @@ class SqlProcessor
 				$key = explode('%', $_key, 2);
 				$subValues[] = $this->processModifier(isset($key[1]) ? $key[1] : 'any', $val);
 			}
-			$values[] = '(' . implode(', ', $subValues) . ')';
+			$values[] = '(' . ($subValues ? implode(', ', $subValues) : 'DEFAULT') . ')';
 		}
 
-		return '(' . implode(', ', $keys) . ') VALUES ' . implode(', ', $values);
+		return ($keys ? '(' . implode(', ', $keys) . ')' : NULL) . ' VALUES ' . implode(', ', $values);
 	}
 
 
@@ -391,7 +391,7 @@ class SqlProcessor
 			$values[] = $this->processModifier(isset($key[1]) ? $key[1] : 'any', $val);
 		}
 
-		return '(' . implode(', ', $keys) . ') VALUES (' . implode(', ', $values) . ')';
+		return ($keys ? '(' . implode(', ', $keys) . ')' : NULL) . ' VALUES (' . ($values ? implode(', ', $values) : 'DEFAULT') . ')';
 	}
 
 
