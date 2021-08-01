99577c6c1af5c1ca453703f30a997d29eedc2945
Jan Skrasek <hrach.cz@gmail.com>
2021-08-01 15:16:09 +0200

    remove not needed assert in DateTimeHelper

--- src/Utils/DateTimeHelper.php
+++ src/Utils/DateTimeHelper.php
@@ -16,7 +16,6 @@ class DateTimeHelper
 	public static function convertToTimezone(DateTimeInterface $value, DateTimeZone $connectionTz): DateTimeInterface
 	{
 		$valueTimezone = $value->getTimezone();
-		assert($value instanceof DateTime || $value instanceof \DateTimeImmutable);
 		assert($valueTimezone !== false); // @phpstan-ignore-line
 
 		if ($valueTimezone->getName() !== $connectionTz->getName()) {
